e0c24b49d53e3b9db093a8a52a29019650739c0e
chetantarale <chetan@shippable.com>
2017-02-08 13:01:06 +0530

    update script

--- firstTest.sh
+++ firstTest.sh
@@ -3,9 +3,19 @@
 # Description: This is a sample test using shunit2
 #
 
-testMyComparison()
-{
-  assertTrue "This is the message if it fails" "[ 1 -eq 1 ]"
+# testMyComparison()
+# {
+#   assertTrue "This is the message if it fails" "[ 1 -eq 1 ]"
+# }
+
+
+#!/bin/bash
+hello() {
+  echo ’Hello, world!’
+}
+test_hello() {
+  assertEquals "$(hello)" ’Hello, world!’
 }
 
+#source "/usr/share/shunit2/shunit2"
 . shunit2
